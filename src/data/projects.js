d31674c4228ffc0ffbf112d4b0d435b6e76a704f
AdrianGerman <adrian.german1019@gmail.com>
2025-01-10 11:01:11 -0600

    data projects and fix home styles

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+const PROJECTS = [
+  {
+    title: "Born - ElMariana Shop",
+    description: "",
+    image: "",
+    link: "#",
+    github: "https://github.com/AdrianGerman/elmariana-shop.git",
+    tags: []
+  },
+  {
+    title: "Heim Components",
+    description: "",
+    image: "",
+    link: "",
+    github: "",
+    tags: []
+  },
+  {
+    title: "Proyectos JS",
+    description: "",
+    image: "",
+    link: "",
+    github: "",
+    tags: []
+  },
+  {
+    title: "Frontend projects roadmap",
+    description: "",
+    image: "",
+    link: "",
+    github: "",
+    tags: []
+  },
+  {
+    title: "Selling books school",
+    description: "",
+    image: "",
+    link: "",
+    github: "",
+    tags: []
+  }
+]
