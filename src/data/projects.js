553fc4d883dd36ecde90434cac78c6ea27fc6181
AdrianGerman <adrian.german1019@gmail.com>
2025-01-12 12:05:40 -0600

    projects tags

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,5 @@
+import { TAGS } from "./tags"
+
 export default [
   {
     title: "Born - ElMariana Shop",
@@ -6,7 +8,7 @@ export default [
     image: "/projects/mariana-shop.webp",
     link: "#",
     github: "https://github.com/AdrianGerman/elmariana-shop.git",
-    tags: []
+    tags: [TAGS.REACT, TAGS.CSS]
   },
   {
     title: "Heim Components",
@@ -14,33 +16,33 @@ export default [
     image: "/projects/heim-components.webp",
     link: "#",
     github: "https://github.com/AdrianGerman/heim-components",
-    tags: []
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.JS]
   },
   {
     title: "Proyectos JS",
     description:
       "Página de proyectos elaborados únicamente con JavaScript, sin librerías ni dependencias.",
     image: "/projects/projects-js.webp",
-    link: "",
+    link: "#",
     github: "https://github.com/AdrianGerman/proyectos-js",
-    tags: []
+    tags: [TAGS.ASTRO, TAGS.JS]
   },
   {
     title: "Frontend projects roadmap",
     description:
       "Roadmap de proyectos front-end, con un total de 24 proyectos, en esta web presenta la línea de proyectos que se elaboraron.",
     image: "/projects/frontend-roadmap.webp",
-    link: "",
+    link: "#",
     github: "https://github.com/AdrianGerman/frontend-projects-roadmap",
-    tags: []
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.REACT]
   },
   {
     title: "Selling books school",
     description:
       "Proyecto personalizado realizado para una institución educativa en la cual realice mi servicio social, venta e inventario de libros escolares.",
     image: "/projects/selling-books.webp",
-    link: "",
+    link: "#",
     github: "https://github.com/AdrianGerman/selling-books-school",
-    tags: []
+    tags: [TAGS.REACT, TAGS.TAILWIND]
   }
 ]
