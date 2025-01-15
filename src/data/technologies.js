9c35ee5198f76165c8f0fe94315f5ddffd3d40f9
AdrianGerman <adrian.german1019@gmail.com>
2025-01-15 08:03:58 -0600

    separating information into js files

---
+++ src/data/technologies.js
@@ -0,0 +1,50 @@
+export const technologies = [
+  {
+    name: "JavaScript",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/js.svg"
+  },
+  {
+    name: "HTML",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/html.svg"
+  },
+  {
+    name: "CSS",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/css.svg"
+  },
+  {
+    name: "Python",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/python.svg"
+  },
+  {
+    name: "Java",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/java.svg"
+  },
+  {
+    name: "React",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/react.svg"
+  },
+  {
+    name: "Vue.js",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/vuejs.svg"
+  },
+  {
+    name: "Astro",
+    icon: "https://miro.medium.com/v2/resize:fit:1400/1*nLbfO_PdTSpeCdZQuUr8RQ.png"
+  },
+  {
+    name: "Nodejs",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png"
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/tailwindcss.svg"
+  },
+  {
+    name: "PHP",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/php.svg"
+  },
+  {
+    name: "MySQL",
+    icon: "https://raw.githubusercontent.com/AdrianGerman/Portfolio/99e5cbe4db594043b8f3a7f839996d1a28fc5580/img/icons/mysql.svg"
+  }
+]
