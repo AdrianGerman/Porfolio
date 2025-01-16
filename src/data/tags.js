17e6a508276494a250085f0411bdab55cdb2dbd7
AdrianGerman <adrian.german1019@gmail.com>
2025-01-16 11:39:04 -0600

    border tAgs and description

--- src/data/tags.js
+++ src/data/tags.js
@@ -8,32 +8,32 @@ import AstroTag from "../components/icons/tags/AstroTag.astro"
 export const TAGS = {
   REACT: {
     name: "React",
-    class: "bg-sky-600",
+    class: "bg-sky-600 border border-sky-900",
     icon: ReactTag
   },
   HTML: {
     name: "HTML",
-    class: "bg-orange-500",
+    class: "bg-orange-500 border border-orange-900",
     icon: HtmlTag
   },
   CSS: {
     name: "CSS",
-    class: "bg-blue-600",
+    class: "bg-blue-600 border border-blue-900",
     icon: CssTag
   },
   JS: {
     name: "JavaScript",
-    class: "bg-yellow-600",
+    class: "bg-yellow-600 border border-yellow-900",
     icon: JsTag
   },
   TAILWIND: {
     name: "TailwindCss",
-    class: "bg-sky-800",
+    class: "bg-sky-800 border border-sky-600",
     icon: TailwindTag
   },
   ASTRO: {
     name: "Astro",
-    class: "bg-black",
+    class: "bg-black border border-slate-600",
     icon: AstroTag
   }
 }
