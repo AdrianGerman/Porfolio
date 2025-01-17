303a2f3c0d3d365552bd2901c4e71ab06359f4e7
AdrianGerman <adrian.german1019@gmail.com>
2025-01-17 10:23:14 -0600

    change title and description of the projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,33 +2,35 @@ import { TAGS } from "./tags"
 
 export default [
   {
-    title: "Born - ElMariana Shop",
+    title: "Born - ElMariana Shop - Tienda online de productos exclusivos.",
     description:
-      "Página web para la venta de mercancía exclusiva por tiempo limitado de un creador de contenido mexicano.",
+      "Página web para la venta de mercancía exclusiva de la empresa, listo para la conexión a shopify para la gestión de los productos y el stock.",
     image: "/projects/mariana-shop.webp",
     link: "https://elmariana-shop.vercel.app/",
     github: "https://github.com/AdrianGerman/elmariana-shop.git",
     tags: [TAGS.REACT, TAGS.CSS]
   },
   {
-    title: "Heim Components",
-    description: "Catálogo de componentes para utilizarlos en tus páginas web.",
+    title: "Heim Components - Catálogo de componentes",
+    description:
+      "Catálogo de componentes creados desde 0 para la implementación en cualquier página web, creados únicamente con HTML y CSS además de la opción para tailwind css.",
     image: "/projects/heim-components.webp",
     link: "https://heim-components.vercel.app/",
     github: "https://github.com/AdrianGerman/heim-components",
     tags: [TAGS.HTML, TAGS.CSS, TAGS.JS]
   },
   {
-    title: "Proyectos JS",
+    title: "Proyectos JS - Proyectos simples pero útiles",
     description:
-      "Página de proyectos elaborados únicamente con JavaScript, sin librerías ni dependencias.",
+      "Página de proyectos elaborados únicamente con JavaScript, sin librerías ni dependencias, para practicar el uso de JS Vanilla.",
     image: "/projects/projects-js.webp",
     link: "https://proyectos-js-teal.vercel.app/",
     github: "https://github.com/AdrianGerman/proyectos-js",
     tags: [TAGS.ASTRO, TAGS.JS]
   },
   {
-    title: "Frontend projects roadmap",
+    title:
+      "Frontend projects roadmap - Todos los ejercicios de la ruta de estudios",
     description:
       "Roadmap de proyectos front-end, con un total de 24 proyectos, en esta web presenta la línea de proyectos que se elaboraron.",
     image: "/projects/frontend-roadmap.webp",
@@ -37,7 +39,7 @@ export default [
     tags: [TAGS.HTML, TAGS.CSS, TAGS.REACT]
   },
   {
-    title: "Selling books school",
+    title: "Selling books school - Gestión de la venta de libros escolares",
     description:
       "Proyecto personalizado realizado para una institución educativa en la cual realice mi servicio social, venta e inventario de libros escolares.",
     image: "/projects/selling-books.webp",
